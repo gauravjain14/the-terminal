147677959b44898964a3b346b55b1b558f9b64b4
gauravjain14 <gjain6@wisc.edu>
2021-10-03 21:19:35 -0700

    local-deploy verified

---
+++ src/Navigation.tsx
@@ -0,0 +1,19 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import React, { FC } from 'react';
+
+const Navigation: FC = () => {
+    const { wallet } = useWallet();
+
+    return (
+        <nav>
+            <h1>The SolTerminal</h1>
+            <div>
+                <WalletMultiButton />
+                {wallet && <WalletDisconnectButton />}
+            </div>
+        </nav>
+    );
+};
+
+export default Navigation;
